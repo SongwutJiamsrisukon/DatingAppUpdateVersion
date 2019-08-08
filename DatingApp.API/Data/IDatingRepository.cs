4d77c4593acee9a01de8ae4891440aae443d3983
songwut <songwut@enoactic.com>
2019-08-08 17:29:43 +0700

    BackEnd Adding Image With cloudinary

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -11,5 +11,6 @@ namespace DatingApp.API.Data
          Task<bool> SaveAll();// when return false is mean no changing to save or error
          Task<IEnumerable<User>> GetUsers();
          Task<User> GetUser(int id);
+         Task<Photo> GetPhoto(int id);
     }
 }
